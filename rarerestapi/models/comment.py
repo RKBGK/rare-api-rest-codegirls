7070fb6856588c64a54cb315ef2919738ee5d306
briannaanelson <briannaanelson@gmail.com>
2022-06-29 17:57:23 -0500

    bri's completed models

---
+++ rarerestapi/models/comment.py
@@ -0,0 +1,9 @@
+from django.db import models
+from rarerestapi.models.post import Post
+from rarerestapi.models.rareuser import RareUser
+
+class Comment(models.Model):
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(RareUser, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+    created_on = models.DateTimeField()
