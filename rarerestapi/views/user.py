b1552c78955921f3e6c4f29aaeed7c6003a6841b
Ruby Kaur <rubiblr@gmail.com>
2022-07-09 12:00:01 -0500

    de-serialization user post

--- rarerestapi/views/user.py
+++ rarerestapi/views/user.py
@@ -2,7 +2,8 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from django.core.exceptions import ValidationError
-from rarerestapi.models.rareuser import RareUser
+from rarerestapi.models import RareUser, Post
+from rest_framework.decorators import action
 
 class RareUserView(ViewSet):
     """User view"""
@@ -16,12 +17,36 @@ class RareUserView(ViewSet):
         users = RareUser.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
+    
+    
+    @action(methods=['get'], detail=True)
+    def userPost(self, request,pk):
+        print("*****************************")
+        print(pk)
+        posts = Post.objects.all().filter(user_id=pk)
+        serializer = UserPostSerializer(posts, many=True)
+        
+        return Response(serializer.data)
+    
 class UserSerializer(serializers.ModelSerializer):
     """JSON serializer for users
     """
+    first_name = serializers.CharField(source = 'user.first_name')
+    last_name = serializers.CharField(source = 'user.last_name')
     class Meta:
         model = RareUser
-        fields = ('user', 'bio', 'image_url', 'created_on', 'active')
-        depth = 1
+
+        fields = ('user', 'bio', 'image_url', 'created_on', 'active','first_name','last_name')
+        # depth = 1
+        
+class UserPostSerializer(serializers.ModelSerializer):
+    """JSON serializer for users
+    """
+    user = UserSerializer(many=False)
+    class Meta:
+        model = Post
+        fields = ('title', 'publication_date','image_url', 'content', 'approved','category','user')
+
+      
 
 
